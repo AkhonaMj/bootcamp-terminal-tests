30ea9270da52a59beb7193c5351b86b8eb79c4d5
AkhonaMj <akhona0304mjikelo@gmail.com>
2023-07-20 15:19:53 +0200

    first commit

---
+++ countRegNumber.js
@@ -0,0 +1,7 @@
+export default function countRegNumber(registrationCount){
+    var regNum = registrationCount.split(",")
+  
+    return regNum.length
+  }
+  
+  
---
+++ test/countRegNumber.test.js
@@ -0,0 +1,13 @@
+import assert from "assert";
+import countRegNumber from "../countRegNumber.js";
+
+describe('Count from all town function', function(){
+
+    it('should count the number of registration numbers', function(){
+        assert.equal(1, countRegNumber('CJ 123'));
+    });
+    it('should count the number of registration numbers', function(){
+        assert.equal(3, countRegNumber('CJ 123, CA 7808, CL 565'));
+    });
+  
+});
